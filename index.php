df2171cc9fa62354ccc32089a1d8bb31ae1a7fc3
MIKO SETIAWAN <98937432+mikosetiawan@users.noreply.github.com>
2022-11-11 16:25:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chatan.com</title>
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- ICON -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body class="bg-light">
+    <nav class="navbar navbar-expand-lg bg-light sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Chatan.com</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="bi bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">You</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container my-5 px-5 ">
+        <div class="row shadow-sm bg-white py-5 px-5 rounded-3" style="height: 300px;">
+            <div class="col-lg-12">
+                <div class="col bg-night rounded-5 text-white px-5 py-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, similique.</div>
+            </div>
+        </div>
+    </div>
+    <div class="container px-5">
+        <div class="row">
+            <div class="col-lg-12">
+                <form action="" method="post">
+                    <div class="row mx-auto">
+                        <div class="col-sm-8 py-2">
+                            <input type="text" class="form-control rounded-5 px-3 py-2" placeholder="Masukan pesan...">
+                        </div>
+                        <div class="col-sm-2 py-2">
+                            <button type="submit" class="bi bi-send btn btn-primary rounded-5"> Kirim</button>
+                        </div>
+                        <div class="col-sm-2">
+                            <img src="assets/img/" alt="">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
